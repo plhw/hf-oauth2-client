8a3944bf9e6ede70f82e4f6ef7c9403823ffc9de
Bas Kamer <bas@bushbaby.nl>
2017-02-27 16:37:13 +0100

    cleanup provider

--- src/Provider/PLHWProvider.php
+++ src/Provider/PLHWProvider.php
@@ -4,44 +4,28 @@ declare(strict_types = 1);
 
 namespace HF\ApiClient\Provider;
 
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Provider\GenericProvider;
+use Psr\Http\Message\ResponseInterface;
 
 class PLHWProvider extends GenericProvider
 {
-    /**
-     * @var string
-     */
-    private $responseError = 'error';
-
-    /**
-     * @var string
-     */
-    private $responseCode;
-
-    /**
-     * @var string
-     */
-    private $accessTokenMethod;
-
-    /**
-     * @var string
-     */
-    private $accessTokenResourceOwnerId;
-
-    /**
-     * @var array|null
-     */
-    private $scopes = null;
-
-    /**
-     * @var string
-     */
-    private $responseResourceOwnerId = 'id';
-
     public function __construct(array $options = [], array $collaborators = [])
     {
         $options['scopeSeparator'] = ' ';
 
         parent::__construct($options, $collaborators);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if (! empty($data['error'])) {
+            $code  = 0;
+            $error = $data['error_description'];
+            throw new IdentityProviderException($error, $code, $data);
+        }
     }
 }

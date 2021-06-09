fc95ee8ea9d16f92e44ca96a69807b5507ad36fe
Bas Kamer <bas@bushbaby.nl>
2021-06-09 12:21:45 +0200

    docs: adds an example to query orders by product group

---
+++ example/dossier/archivedOrdersForSandalsCustomMade.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Project 'Healthy Feet' by Podolab Hoeksche Waard.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see       https://plhw.nl/
+ *
+ * @copyright Copyright (c) 2010 - 2021 bushbaby multimedia. (https://bushbaby.nl)
+ * @author    Bas Kamer <baskamer@gmail.com>
+ * @license   Proprietary License
+ *
+ * @package   plhw/hf-api-client
+ */
+
+declare(strict_types=1);
+
+use HF\ApiClient\ApiClient;
+use HF\ApiClient\Exception\ClientException;
+use HF\ApiClient\Exception\GatewayException;
+use HF\ApiClient\Query\Query;
+
+/** @var $api ApiClient */
+require_once __DIR__ . '/../setup.php';
+
+try {
+    $dossierNumber = '10003';
+    $productGroupCode = 'S:CM';
+
+    $results = $api->dossier_queryOrders(
+        Query::create()
+            ->withFilter('query', $dossierNumber) // filter by on dossier.dossierNumber
+            ->withFilter('status', 'archived') // sort order.status (OPENED, ARCHIVED, DELETED)
+            ->withFilter('productGroup', $productGroupCode)
+            ->withSort('placementDate', false)
+            ->withPage(1, 10)
+    );
+} catch (ClientException $e) {
+    \printf("%s\n\n", $e->getMessage());
+    exit();
+} catch (GatewayException $e) {
+    \printf("%s\n\n", $e->getMessage());
+    \printf('%s', $api->getLastResponseBody());
+    exit();
+} catch (\Exception $e) {
+    \printf("%s\n\n", $e->getMessage());
+} finally {
+    if ($api->isSuccess()) {
+        // do something with $results (which is the parsed response object)
+        dump($results);
+
+        // or do something with $api->cachesResources (which contains a (flattened) array of json-api resources by resource type type)
+        dump($api->cachedResources);
+
+
+        \printf("Archived orders from '%s' and dossier number %s\n", $productGroupCode, $dossierNumber);
+
+        foreach ($results['data'] as $order) {
+            \printf("Ordernumber %s, placed on: %s\n", $order['attributes']['order-number'], $order['attributes']['placement-date'] ?: 'not-placed');
+        }
+    }
+}
